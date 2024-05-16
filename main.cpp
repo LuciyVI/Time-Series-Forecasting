7e40298938d2dfaa3a239e6161c2b6ae2feb3b65
fb <fb@kali.kali>
2024-05-16 22:18:25 +0300

    update build func

--- main.cpp
+++ main.cpp
@@ -47,10 +47,10 @@ void distribution_weights(Eigen::VectorXd &weights, Eigen::VectorXd input,Eigen:
 
         for(int i = 0; i<weights.size()-1;++i)
         {
-            
+            //TODO передать изменённые веса в main
             double error = calculate_error(input(i),right_value(i));
-            std::cout<<"Error : "<<error<<std::endl;
             weights(i+1)+=weights(i)*(delta_wieghts(error,input(i)));
+            std::cout<<"w : "<<weights<<std::endl;
             
             sum_error += error*error;
             
@@ -61,11 +61,11 @@ void distribution_weights(Eigen::VectorXd &weights, Eigen::VectorXd input,Eigen:
 
 
 
-void ploting_grph(double y , double x, Eigen::VectorXd input){
+void ploting_grph(double y , double x, std::vector<double> input){
 
     Gnuplot gp;
-    gp << "set xrange [-5:5]\nset yrange [-5:5]\n";
-    gp << "plot '-' with lines title 'y = x^2'\n";
+    gp << "set xrange [y:x]\nset yrange [y:x]\n";
+    gp << "plot '-' with lines title 'func = 0.5*cos(0.5*x)-0.5 \n";
     gp.send1d(input);
 
 }
@@ -80,34 +80,50 @@ int main() {
     double a = -5.0;
     double b = 5.0;
     
-    int len_window = 6;
+    int len_window = 4;
 
-    Eigen::VectorXd wights(len_window);
-    Eigen::VectorXd right_vector(number_points);
+    std::vector<double> wights(len_window);
+    
     Eigen::VectorXd input(number_points);
     Eigen::VectorXd new_answer(number_points);
     Eigen::VectorXd X(1000);
     Eigen::VectorXd Y(1000);
     int count = 1;
+    for(int i = 0;i<len_window;i++)
+    {
+        wights[i]=1.0;
+    }
     
-    do{
-        
-     
-        input(count-1)=a+(count-1)*(b-a)/number_points;
-        right_vector(count-1)=linearActivation(input(count-1));
-        input((((input.size()-2)/2)+count))=(b+(count-1))*((b-a)/number_points);
-        new_answer(count-1)=linearActivation(input((input.size()-1)/2+count));
-        count++;
-        
-        
+
+    std::vector<double>inputs_vector;
+    Eigen::VectorXd inputs_eigen(number_points+1);
+    std::vector<double>right_vector;
+    
+
+    for (int i = 0; i < number_points+1; ++i) {
+        inputs_vector.push_back(a + i * (b - a) / number_points);
+        inputs_eigen(i) = a + i * (b - a) / number_points;
+        right_vector.push_back(linearActivation(inputs_eigen(i)));
+    }  
+
+    std::cout << "Using vector with push_back:" << std::endl;
+        for (const auto &input : inputs_vector) {
+            std::cout << input << " ";
+
+        }
+    std::cout << std::endl;
+
+    std::cout << "Using Eigen::VectorXd:" << std::endl;
+    std::cout << inputs_eigen << std::endl;
+
+    std::cout << "Right vector with push_back:" << std::endl;
+        for (const auto &right_value : right_vector) {
+         
+            std::cout<<"\n"<< right_value << " ";
+            
+        }
 
 
-    }while(!input.all());
-        
-        std::cout<<"Right_Vector : "<<right_vector<<std::endl;
-        std::cout<<"inputs :  "<<input<<std::endl;
-        std::cout<<"new_answer :  "<<new_answer<<std::endl;
-    
     for(int i = 0; i<1000;i++){
         X(i)=a-0.5+i*(1+2*b-a)/100;
     }
@@ -115,17 +131,22 @@ int main() {
         Y(i)=linearActivation(X(i));
     }
     
+
+    std::cout<<"W[0] "<<wights[0]<<"\t W[1]"<<wights[1]<<"\t W[2]"<<wights[2]<<"\t W[3]"<<wights[3]<<std::endl;
     double sum_error = 0.0;
     double *ptr  = &sum_error;
 
+
+    std::cout<<"Sum_error : "<<sum_error<<std::endl; 
+
+    ploting_grph(4,5,right_vector);
+        
+        std::cout<<"Sum_error : "<<sum_error<<std::endl;        
+        // distribution_weights(wights,input,right_vector,len_window,number_points,*ptr);
+        
+        // std::cout<<"Net:"<<net(wights,input,len_window)<<std::endl;
     
-
-    // distribution_weights(wights,input,right_vector,len_window,number_points,*ptr);
-
-    // ploting_grph(4,5,Y);
-
-    // std::cout<<"Net:"<<net(wights,input,len_window)<<std::endl;
-
+    
     return 0;
 
 }
